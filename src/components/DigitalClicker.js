3c2612f28cf74e3d32b5f4983bd225621cb09b7f
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-09-10 08:55:37 +0000

    automatically backed up by learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React from 'react';
 
 class DigitalClicker extends React.Component {
@@ -23,3 +24,25 @@ class DigitalClicker extends React.Component {
 }
 
 export default DigitalClicker;
+=======
+// Code DigitalClicker Component Here
+
+import React from 'react'; 
+
+class DigitalClicker extends React.Component{
+    constructor() {
+    super();
+     this.state = {
+      timesClicked: -1 };
+  }
+  
+  render(){
+    return(
+       <button onclick="" > {this.state.timesClicked } have ben clicked </button>
+      )
+  }
+}
+
+
+export default DigitalClicker ;
+>>>>>>> e83e0b1d0c7f1bf350cd99126ae71358e49047cb
