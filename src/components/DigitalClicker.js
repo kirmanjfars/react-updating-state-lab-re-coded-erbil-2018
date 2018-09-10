1077c60de36b428385781b283c5a3acb5ddeac02
Kirmanj Faris <kirmanj.00806510@gmail.com>
2018-09-10 08:49:40 +0000

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,19 @@
 // Code DigitalClicker Component Here
+
+import React from 'react'; 
+
+class DigitalClicker extends React.Component{
+    constructor() {
+    super();
+     this.state = {
+      timesClicked: -1 };
+  }
+  render(){
+    return(
+       <button onclick="" > {this.state.timesClicked } have ben clicked </button>
+      )
+  }
+}
+
+
+export default DigitalClicker ;
--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -1,1 +1,38 @@
 // Code YouTubeDebugger Component Here
+
+import React from 'react'; 
+
+class YouTubeDebugger  extends React.Component{
+    constructor() {
+    super();
+     this.state = { 
+       errors: [], 
+       user: null, 
+       settings: { bitrate: 8, video: { resolution: '1080p' } } 
+     }}
+     
+     bitrate(){
+       this.setState{
+         settings.bitrate = 12
+       }
+     }
+     
+     resolution(){
+        this.setState{
+         settings.video.resolution = '720p'
+       }
+     }
+  
+  render(){
+    return(
+      <div>
+      <button class="bitrate" onclick="bitrate">{this.state}  </button>
+      <button class="resolution" onclick="resolution">{this.state}   </button>
+      </div>
+      )
+  }
+}
+
+
+export default DigitalClicker ;
+
